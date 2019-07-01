3adce0a655a6cf3a912e2055285b99743d6a8c0a
igorjv123 <igorjv123@gmail.com>
2019-07-02 02:07:20 +0300

    add SASS to GULP

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,16 @@ const browsersync = require("browser-sync").create();
 const del = require("del");
 const gulp = require("gulp");
 const merge = require("merge-stream");
+var sass = require('gulp-sass');
+var htmlImport = require('gulp-html-import');
+
 
 // BrowserSync
+function importHtml(){
+  return gulp.src('./index.html')
+  .pipe(htmlImport('./components/random/random.html'))
+  .pipe(gulp.dest('/build/index.html')); 
+}
 function browserSync(done) {
   browsersync.init({
     server: {
@@ -16,13 +24,27 @@ function browserSync(done) {
   });
   done();
 }
-
+var paths = {
+  styles: {
+      // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+      src: "./**/*.sass",
+      // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+      dest: "./"
+  }
+};
 // BrowserSync reload
+
 function browserSyncReload(done) {
   browsersync.reload();
   done();
 }
-
+function style() {
+  return gulp
+      .src(paths.styles.src)
+      .pipe(sass())
+      .on("error", sass.logError)
+      .pipe(gulp.dest(paths.styles.dest));
+}
 // Clean vendor
 function clean() {
   return del(["./vendor/"]);
@@ -46,6 +68,8 @@ function modules() {
 function watchFiles() {
   gulp.watch("./**/*.css", browserSyncReload);
   gulp.watch("./**/*.html", browserSyncReload);
+  gulp.watch('./**/*.sass', style)
+  gulp.watch('./',importHtml)
 }
 
 // Define complex tasks
@@ -59,3 +83,6 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+exports.style = style;
+exports.importHtml = importHtml;
+
