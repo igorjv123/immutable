234e4291450beade9f3a176fc33b6de68b27835d
igorjv123 <Ihor_Krutenko@epam.com>
2019-07-11 14:43:13 +0300

    add components dir

---
+++ src/components/login/index.js
@@ -0,0 +1,5 @@
+import Login from './login';
+import './login.sass';
+
+const login = new Login();
+login.init();
---
+++ src/components/login/login.js
@@ -0,0 +1,7 @@
+import pugHtml from './login.pug';
+
+class Login {
+  pugHtml(params);
+}
+
+export default Login;
