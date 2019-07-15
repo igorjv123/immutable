9d2134cb32f79d444d9d44a3fcf40d8e272489c4
igorjv123 <Ihor_Krutenko@epam.com>
2019-07-15 18:19:13 +0300

    fix sign component ui

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -3,3 +3,23 @@ import './login.sass';
 
 const login = new Login();
 login.init();
+//
+// $(() => {
+//     const $loginForm = $('.login-form');
+//
+//     $loginForm.each((i, el) => {
+//         const instance = new LoginForm({element: el});
+//         instance.init();
+//     });
+// });
+//
+// class LoginForm {
+//     constructor({element}) {
+//         this.$el = $(element);
+//         this.el = this.$el[0];
+//     }
+//
+//     init() {
+//         this.$el.on('click', '.form-input', console.log);
+//     }
+// }
