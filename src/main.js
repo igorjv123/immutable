d4b9c92ee28026d3aa718b9b414546abe86e573a
Vladyslav_Hybalo <vladhybalo@gmail.com>
2019-07-15 11:29:52 +0300

    registrarion with fb and google

--- src/main.js
+++ src/main.js
@@ -27,13 +27,21 @@ document.querySelector('.user-info__btn').addEventListener('click', logout);
             console.log('Signed in');
             console.dir(user);
             // show logout btn
-            document.querySelector('.user-info__btn').innerText = 'Login';
+            document.querySelector('.user-info__btn').innerText = 'Logout';
             document.querySelector('.user-info__avatar').src = user.photoURL;
+
+            document.querySelector('.user-info__sign-in').classList.remove('hide');
+            document.querySelector('.user-info__sign-in').classList.add('show');
+
         } else {
             // No user is signed in.'
             console.log("Already signed out");
             // show login btn
-            // document.querySelector('.user-info__btn').innerText = 'Login';
+            document.querySelector('.user-info__btn').innerText = 'Login';
+            document.querySelector('.user-info__avatar').classList.remove('user-signed');
+
+            document.querySelector('.user-info__sign-in').classList.remove('show');
+            document.querySelector('.user-info__sign-in').classList.add('hide');
         }
         document.querySelector('.user-info').classList.add('show')
     });
--- src/scripts/userRegistrarion.js
+++ src/scripts/userRegistrarion.js
@@ -109,7 +109,7 @@ function signUpWithFb() {
             var token = result.credential.accessToken;
             // The signed-in user info.
             var user = result.user;
-
+            console.dir(user);
             // ...
         })
         .catch(function (error) {
