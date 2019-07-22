373f03682d58aaaa9bb5536221198b9f77e9bdc4
igorjv123 <ihor_krutenko@epam.com>
2019-07-22 10:20:54 +0300

    1qwe

--- src/scripts/initUser.js
+++ src/scripts/initUser.js
@@ -1,5 +1,5 @@
 import * as firebase from 'firebase/app';
-import { InitUserObserver, InitUserObservable } from './InitUserObserver';
+import {InitUserObserver, InitUserObservable} from './InitUserObserver';
 
 const userInfoButtonObserver = new InitUserObserver(changeUserBtnLoginSuccses);
 const userInfoAvatarObserver = new InitUserObserver(changeAvatarLoginSuccses);
@@ -22,43 +22,43 @@ userErrorObservable.subscribe(userErrorinfoObserver);
 userInfoAvatarObserver.unsubscribe()
 
 function initUser() {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      console.log('Signed in');
-      console.dir(user);
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            console.log('Signed in');
+            console.dir(user);
 
-      userInfoObservable.notify(user.photoURL);
-    } else {
-      console.log('Already signed out');
+            userInfoObservable.notify(user.photoURL);
+        } else {
+            console.log('Already signed out');
 
-      userErrorObservable.notify();
-    }
-    $('.user-info').addClass('show');
-  });
+            userErrorObservable.notify();
+        }
+        $('.user-info').addClass('show');
+    });
 }
 
 function changeUserBtnLoginFailed() {
-  $('.user-info__btn').text('Login');
+    $('.user-info__btn').text('Login');
 }
 
 function changeAvatarLoginFailed() {
-  $('.user-info__avatar').removeClass('user-signed');
+    $('.user-info__avatar').removeClass('user-signed');
 }
 
 function changeUserInfoLoginFailed() {
-  $('.user-info__sign-in').removeClass('show');
+    $('.user-info__sign-in').removeClass('show');
 }
-
 
 function changeUserBtnLoginSuccses() {
-  $('.user-info__btn').text('Logout');
+    $('.user-info__btn').text('Logout');
 }
+
 function changeAvatarLoginSuccses(url) {
-  console.log(url);
-  $('.user-info__avatar').attr('src', url);
+    $('.user-info__avatar').attr('src', url);
 }
+
 function changeUserInfoLoginSucces() {
-  $('.user-info__sign-in').addClass('show');
+    $('.user-info__sign-in').addClass('show');
 }
 
 export default initUser;
